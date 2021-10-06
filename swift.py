6452e6be22f2e5ee93fbfc90dc5c4157e52e6c86
amber-cocch <57418017+amber-cocch@users.noreply.github.com>
2021-10-06 15:19:30 -0400

    Add files via upload

--- swift.py
+++ swift.py
@@ -125,6 +125,7 @@ def delete_task():
     response.headers['Content-Type'] = 'application/json'
     return json.dumps({'success': True})
 
+
 if __name__ == "__main__":
 
     run(host='0.0.0.0', port=8080, debug=True)
